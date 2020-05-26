5e4aba5b7c1af1f803c2249abd219da0228a2f35
monestri <63887617+monestri@users.noreply.github.com>
2020-05-25 21:51:35 -0400

    Test update.

--- src/index.ts
+++ src/index.ts
@@ -289,7 +289,7 @@ client.on('message', async (message: Discord.Message) => {
         "**!inventory [username]** \n\t List all ships a certain user owns. Leave blank for your own\n" +
         "**!fleetview {user|-org}** \n\t Generate a fleetview.json file for the org or a user.\n" +
         "**!import** \n\t Upload a HangarXPLOR or FleetView JSON File and specify this command in the comment.\n" +
-        "**!stats** \n\t Org fleet statistics.\n"
+        "**!stats** \n\t Display org fleet statistics.\n"
 
       if (hasRole(message, "Management")){
         msg += "**!removeall _user#xxxx_** \n\t (Management): Delete all data for a user.\n"
