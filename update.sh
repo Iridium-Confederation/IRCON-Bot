960a18509e9ae16ad87c9282efa9d7611a159a2f
monestri <63887617+monestri@users.noreply.github.com>
2020-08-12 23:01:54 -0400

    Stop old bot.

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-SERVICE_LOC=$(systemctl status ircon-bot | grep -Po "(?<=loaded \()[^;]+")
-WORKING_DIR=$(grep -Po "(?<=WorkingDirectory=).*" $SERVICE_LOC)
-cd $WORKING_DIR
-git reset --hard
-git pull
-npm install
-npm run build
-sudo systemctl restart ircon-bot
+#SERVICE_LOC=$(systemctl status ircon-bot | grep -Po "(?<=loaded \()[^;]+")
+#WORKING_DIR=$(grep -Po "(?<=WorkingDirectory=).*" $SERVICE_LOC)
+#cd $WORKING_DIR
+#git reset --hard
+#git pull
+#npm install
+#npm run build
+#sudo systemctl restart ircon-bot
+
+sudo systemctl stop ircon-bot
