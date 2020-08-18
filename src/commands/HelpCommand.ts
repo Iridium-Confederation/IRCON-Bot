b9a1e11e302869c3951684733160f57913d14049
monestri <monestri@users.noreply.github.com>
2020-08-17 22:04:02 -0400

    Clean up help menu.

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -11,13 +11,13 @@ export const HelpCommand: FleetBotCommand = async (
 
   let msg =
     "Looking for the exact name of your ship? See: <https://fleetyards.net/ships/>\n\n" +
-    "**!fb add <ship>** \n\t Add a ship to your fleet.\n" +
-    "**!fb remove {ship|-all}** \n\t Remove ships from your fleet.\n" +
-    "**!fb search <ship>** \n\t List all owners of a certain ship.\n" +
-    "**!fb inventory [username]** \n\t List all ships a certain user owns. Leave blank for your own\n" +
-    "**!fb fleetview {user|-org}** \n\t Generate a fleetview.json file for the org or a user.\n" +
+    "**!fb add SHIP** \n\t Add a ship to your fleet.\n" +
+    "**!fb remove {SHIP|-all}** \n\t Remove ships from your fleet.\n" +
+    "**!fb search SHIP** \n\t List all owners of a certain ship.\n" +
+    "**!fb inventory [USER]** \n\t List all ships a certain user owns. Leave blank to show your personal inventory.\n" +
+    "**!fb fleetview [USER|-org]** \n\t Generate a fleetview.json file for the org or a user. Leave blank to generate your personal FleetView.\n" +
     "**!fb import** \n\t Upload a HangarXPLOR or FleetView JSON File and specify this command in the comment.\n" +
-    "**!fb stats [ship]** \n\t Display org fleet statistics or show detailed info about a single ship.\n";
+    "**!fb stats [SHIP]** \n\t Display org fleet statistics or show detailed info about a single ship. Leave blank to show org statistics.\n";
 
   msg +=
     "**!fb removeall _user#xxxx_** \n\t (Management Role): Delete all data for a user.\n";
