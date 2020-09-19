273a0edb684a6e5f393e0584e082f690cc3131b1
monestri <monestri@users.noreply.github.com>
2020-09-18 21:05:48 -0400

    Add org lookup command

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -14,7 +14,7 @@ export const HelpCommand: FleetBotCommand = async (
     `**${PREFIX()}add SHIP** \n\t Add a ship to your fleet.\n` +
     `**${PREFIX()}remove {SHIP|-all}** \n\t Remove ships from your fleet.\n` +
     `**${PREFIX()}search SHIP** \n\t List all owners of a certain ship.\n` +
-    `**${PREFIX()}inventory [USER]** \n\t List all ships a certain user owns. Leave blank to show your personal inventory.\n` +
+    `**${PREFIX()}inventory [USER|-org]** \n\t List all ships a certain user or the org owns. Leave blank to show your personal inventory.\n` +
     `**${PREFIX()}fleetview [USER|-org]** \n\t Generate a fleetview.json file for the org or a user. Leave blank to generate your personal FleetView.\n` +
     `**${PREFIX()}import** \n\t Upload a HangarXPLOR or FleetView JSON File and specify this command in the comment.\n` +
     `**${PREFIX()}stats [SHIP]** \n\t Display org fleet statistics or show detailed info about a single ship. Leave blank to show org statistics.\n`;
