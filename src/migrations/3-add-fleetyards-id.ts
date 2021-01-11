a7a51f284dcea8b39ed11efad48da6ad4cd13320
monestri <63887617+monestri@users.noreply.github.com>
2021-01-10 23:29:26 -0500

    Comment out old migration.

--- src/migrations/3-add-fleetyards-id.ts
+++ src/migrations/3-add-fleetyards-id.ts
@@ -3,8 +3,8 @@ import { QueryInterface } from "sequelize";
 
 module.exports = {
   async up(queryInterface: QueryInterface, sequalize: any) {
-    return queryInterface.addColumn("ships", "fleetyardsId", {
-      type: DataType.STRING,
-    });
+    // return queryInterface.addColumn("ships", "fleetyardsId", {
+    //   type: DataType.STRING,
+    // });
   },
 };
